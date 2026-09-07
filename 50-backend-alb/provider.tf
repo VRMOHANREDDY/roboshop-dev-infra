eceaf1ce356c0d9a69e4f876d9ce39c482f9fa42
Ramamohan.Vootukuri <vrmohanr@gmail.com>
2026-09-07 16:19:44 -0400

    update

---
+++ 50-backend-alb/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.48.0"
+    }
+  }
+
+    backend "s3" {
+    bucket         = "remote-ram-devops"
+    key            = "roboshop-backend-alb-tfstate"  # s3 bucket created with this name
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true # Activates native S3 state locking
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
