430d11d3d961e6d4ff1523eb28fbd8d08da535a0
Ramamohan.Vootukuri <vrmohanr@gmail.com>
2026-09-07 18:14:25 -0400

    error

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,22 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+/* variable "zone_id" {
+    default = "Z079062128O2JVDPFINH5"   # get it form hosted zone ramvootukuri.com
+}  */ 
+
+variable "domain_name" {
+    default = "ramvootukuri.com"
+}
+
+
+
