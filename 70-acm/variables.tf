c567b5b977e47d60f86f890aedb7eb41faac40b0
Ramamohan.Vootukuri <vrmohanr@gmail.com>
2026-09-08 12:51:52 -0400

    polciy

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "ramvootukuri.com"
+}
+
+variable "zone_id" {
+    default = "Z079062128O2JVDPFINH5"
+}  
