64d54c47756368bd0d9160bc1f649f181ac8b2a6
Ramamohan.Vootukuri <vrmohanr@gmail.com>
2026-09-08 13:12:27 -0400

    falb

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z079062128O2JVDPFINH5"
+}  
+
+variable "domain_name" {
+    default = "ramvootukuri.com"
+}
