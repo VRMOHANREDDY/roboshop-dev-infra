eb4072109806714c5a2cd9813ce10a2d9a858625
Ramamohan.Vootukuri <vrmohanr@gmail.com>
2026-09-15 10:20:54 -0400

    cdn

---
+++ 95-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "zone_id" {
+    default = "Z08730091OG15WN76ZKZS"
+}  
+
+variable "domain_name" {
+    default = "ramvootukuri.com"
+}
